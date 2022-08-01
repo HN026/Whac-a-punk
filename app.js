1e5bdb5d73b7997ce6b9d3fe0860d5626acedfa6
HN26 <plancksconstant01@gmail.com>
2022-08-01 16:54:17 +0530

    Third Commit

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
+const level = document.querySelector('#level');
 
 let result = 0;
 let hitPosition;
 let currentTime = 60;
 let timerId = null;
+let Level = 1;
 
 function randomSquares(){
     squares.forEach(square => {
@@ -45,16 +47,59 @@ moveMole();
 function countDown(){
  currentTime--;
  timeLeft.textContent = currentTime;
+
+
+ if(currentTime == 45)
+ {
+    IncreaseLevel2();
+ }
+
+ if(currentTime == 25)
+ {
+    IncreaseLevel3();
+ }
+ 
  
  if (currentTime == 0)
  {
     clearInterval(countDownTimerId);
     clearInterval(timerId);
-    alert('GAME OVER! Your final score is ' + result);
+    if(result>500)
+    {
+        alert('GAME OVER! You won, Your score is: ' + result);
+    }
+    else
+    {
+        alert('GAME OVER! You lost, Your score is: ' + result);
+    }
  }
   
 
 }
+
+function IncreaseLevel2()
+{
+    if(result>100)
+    {
+        timerId = setInterval(randomSquares, 200);
+        Level++;
+        level.textContent = Level;
+
+    }
+}
+
+function IncreaseLevel3()
+{
+    if(result>300)
+    {
+        timerId = setInterval(randomSquares, 300);
+        Level++;
+        level.textContent = Level;
+
+    }
+}
+
+
 
 let countDownTimerId = setInterval(countDown,1000)
 
